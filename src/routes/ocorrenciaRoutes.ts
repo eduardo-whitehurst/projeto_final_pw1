e67ee578e67daa6f7fee1b570af3329f1591b047
AntLacerda <lacerdaneto99@hotmail.com>
2023-12-13 20:53:11 -0300

    Implementando upload de imagens

--- src/routes/ocorrenciaRoutes.ts
+++ src/routes/ocorrenciaRoutes.ts
@@ -1,14 +1,17 @@
 import { Router } from "express";
 import { ocorrenciaController } from "../controllers/ocorrenciaController";
+import uploadsConfig from "../config/multer";
+import multer from "multer";
 import { autenticacao } from "../middlewares/autenticacao";
 
 const router = Router();
+const upload = multer(uploadsConfig);
 
 router.use(autenticacao);
 
 router.get("/", ocorrenciaController.buscarTodasOcorrencias);
 router.get("/:id", ocorrenciaController.buscarOcorrenciaPeloId);
-router.post("/", ocorrenciaController.criarOcorrencia);
+router.post("/", upload.array("images"),ocorrenciaController.criarOcorrencia);
 router.put("/:id", ocorrenciaController.editarOcorrencia);
 router.delete("/:id", ocorrenciaController.deletarOcorrencia);
 
